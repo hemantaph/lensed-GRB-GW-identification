e35c693a4ed9f436db080abd40d1a590844d5401
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2025-05-08 14:54:04 +0800

    Updated

--- data/bayesfactors.py
+++ data/bayesfactors.py
@@ -1,1 +1,53 @@
 import numpy as np
+
+# TODO : Triplets/quaduplets
+class BayesFactorPairs:
+    def __init__(self, data):
+        """
+        Initialize the BayesFactors class with data.
+
+        Parameters:
+        data (array-like): The data to be used for calculating Bayes factors.
+        """
+        self.data = np.array(data)
+        self.log_bayes_factors = {}
+        self.log_bayes_factors['sky'] = None
+        self.log_bayes_factors['td'] = None
+        self.log_bayes_factors['mur'] = None
+        self.log_bayes_factors['spectral'] = None
+        self.log_bayes_factors['total'] = None
+    
+    def calculate_sky_bayes_factors(self):
+        """
+        Calculate the Bayes factors for the sky model.
+        """
+        # Placeholder for actual calculation
+        raise NotImplementedError("Sky Bayes factor calculation not implemented.")
+    
+    def calculate_td_bayes_factors(self):
+        """
+        Calculate the Bayes factors for the time-domain model.
+        """
+        # Placeholder for actual calculation
+        raise NotImplementedError("Time-domain Bayes factor calculation not implemented.")
+    
+    def calculate_mur_bayes_factors(self):
+        """
+        Calculate the Bayes factors for the multi-band model.
+        """
+        # Placeholder for actual calculation
+        raise NotImplementedError("Multi-band Bayes factor calculation not implemented.")
+    
+    def calculate_spectral_bayes_factors(self):
+        """
+        Calculate the Bayes factors for the spectral model.
+        """
+        # Placeholder for actual calculation
+        raise NotImplementedError("Spectral Bayes factor calculation not implemented.")
+    
+    def calculate_total_bayes_factors(self):
+        """
+        Calculate the total Bayes factors.
+        """
+        # Placeholder for actual calculation
+        raise NotImplementedError("Total Bayes factor calculation not implemented.")
